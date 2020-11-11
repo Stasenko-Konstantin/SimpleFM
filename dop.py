afc50d383261e0967af8899d5d97f2065a93fc52
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2020-11-11 23:30:56 +0500

    second commit

--- dop.py
+++ dop.py
@@ -1,3 +1,5 @@
+#! Модуль вспомогательных функций
+
 def sortdir(arg):
     x1, x2 = [i for i in arg if not "." in i], [i for i in arg if "." in i]
     return x1 + x2
--- fm.py
+++ fm.py
@@ -1,32 +1,39 @@
+#! Главный модуль
+
+'''
+Автор: Стасенко Константин
+
+О программе: простой файловый менеджер который необходимо было сделать по учебному
+заданию
+'''
+
 from tkinter import *
 from os import *
-from dop import *
+import dop # Мой дополнительный модуль
 
 stdpath = getenv("SystemDrive")+"\\"
-d = sortdir(listdir(path=stdpath))
-print(d)
+stdlist = dop.sortdir(listdir(path=stdpath))
 
 def upd(event):
     updd()
 
 def updd():
     global left, right
-    left.config(width=root.winfo_width()/3, height=root.winfo_height()-40)
-    right.config(width=root.winfo_width(), height=root.winfo_height()-40)
-    left.place(x=0, y=40)
-    right.place(x=root.winfo_width()/3, y=40)
+    left.config(height=root.winfo_height()-40)
+    right.config(height=root.winfo_height()-40)
 
 root = Tk()
 root.geometry("1000x600")
 root.minsize(1000, 600)
 root.title("stdpath")
 
-left = Frame(root, width=root.winfo_width()/3, height=root.winfo_height()-40, bg="gray76")
-right = Frame(root, width=root.winfo_width(), height=root.winfo_height()-40, bg="lemon chiffon")
+
+left = Frame(root, width=333, height=560, bg="gray76")
+right = Frame(root, width=4000, height=560, bg="lemon chiffon")
 
 
 left.place(x=0, y=40)
-right.place(x=root.winfo_width()/3, y=40)
+right.place(x=333, y=40)
 
 root.bind('<Configure>', upd)
 
