c5f8410c5415d16c71d6ae40df1390e0e31f5753
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2020-11-16 22:02:45 +0500

    several btns

--- dop.py
+++ dop.py
@@ -1,5 +1,9 @@
 #! Модуль вспомогательных функций
 
 def sortdir(arg):
-    x1, x2 = [i for i in arg if not "." in i], [i for i in arg if "." in i]
+    x1, x2 = [i for i in arg if not "." in i]
+        , [i for i in arg if "." in i]
     return x1 + x2
+
+def concat(arg):
+    return arg + "\\"
--- fm.py
+++ fm.py
@@ -11,30 +11,75 @@ from tkinter import *
 from os import *
 import dop # Мой дополнительный модуль
 
-stdpath = getenv("SystemDrive")+"\\"
+stdpath = dop.concat(getenv("SystemDrive"))
 stdlist = dop.sortdir(listdir(path=stdpath))
+btn_list = []
 
-def upd(event):
-    updd()
+execute = "\n\
+global {0}\n\
+def left_click{1}(event):\n\
+    global stdpath, stdlist\n\
+    if '.' in stdlist[{1}]:\n\
+        pass\n\
+    else:\n\
+        stdpath = dop.concat(path.abspath(stdpath + stdlist[{1}]))\n\
+        print(stdpath)\n\
+        stdlist = dop.sortdir(listdir(path=stdpath))\n\
+        destr(btn_list, stdlist)\n\
+        root.title(stdpath)\n\
+{0} = Button(right, text=stdlist[i], bg='old lace')\n\
+{0}.bind('<Button-1>', left_click{1})\n\
+{0}.place(x=10, y=rast)\n\
+"
 
-def updd():
+def per(event):
+    upd()
+
+def upd():
     global left, right
-    left.config(height=root.winfo_height()-40)
-    right.config(height=root.winfo_height()-40)
+    
+    left.config(height=root.winfo_height()-80)
+    right.config(height=root.winfo_height()-80)
 
+    rast = 5
+    for i in range(len(stdlist)):
+        name_btn = "btn" + str(i)
+        exec(
+"{0}.place(x=10, y=rast)\n\
+".format(name_btn))
+        rast += 30
+
+def destr(x, y):
+    global rast
+    rast = 5
+    for i in x:
+        exec(
+"{0}.destroy()\n\
+".format(i))
+    for i in range(len(y)):
+        name_btn = "btn" + str(i)
+        btn_list.append(name_btn)
+        exec(execute.format(name_btn, i))
+        rast += 30
+        
 root = Tk()
 root.geometry("1000x600")
 root.minsize(1000, 600)
-root.title("stdpath")
-
+root.title(stdpath)
 
 left = Frame(root, width=333, height=560, bg="gray76")
 right = Frame(root, width=4000, height=560, bg="lemon chiffon")
 
+left.place(x=0, y=80)
+right.place(x=333, y=80)
 
-left.place(x=0, y=40)
-right.place(x=333, y=40)
+rast = 5
+for i in range(len(stdlist)):
+    name_btn = "btn" + str(i)
+    btn_list.append(name_btn)
+    exec(execute.format(name_btn, i))
+    rast += 30
 
-root.bind('<Configure>', upd)
+root.bind('<Configure>', per)
 
 root.mainloop()
