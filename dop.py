1f5d07c764e5e8192c90e51b7fa613b9c2239650
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2020-11-16 22:04:50 +0500

    -1 stupid error

--- dop.py
+++ dop.py
@@ -1,8 +1,7 @@
 #! Модуль вспомогательных функций
 
 def sortdir(arg):
-    x1, x2 = [i for i in arg if not "." in i]
-        , [i for i in arg if "." in i]
+    x1, x2 = [i for i in arg if not "." in i], [i for i in arg if "." in i]
     return x1 + x2
 
 def concat(arg):
