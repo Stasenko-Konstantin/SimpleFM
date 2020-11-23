011903c6065fe35a267f5ff300179e0d486f4ce8
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2020-11-23 16:37:00 +0500

    back

--- dop.py
+++ dop.py
@@ -1,5 +1,40 @@
 #! Модуль вспомогательных функций
 
+def exec():
+    return "\n\
+global {0}, {2}\n\
+def left_click{1}(event):\n\
+    global stdpath, stdlist\n\
+    if '.' in stdlist[{1}]:\n\
+        pass\n\
+    else:\n\
+        try:\n\
+            stdpath = dop.concat(path.abspath(stdpath + stdlist[{1}]))\n\
+            print(stdpath)\n\
+            btn_list = dop.btn_lists(stdlist)\n\
+            radio_list = dop.radio_lists(stdlist)\n\
+            stdlist = dop.sortdir(listdir(path=stdpath))\n\
+            destr(btn_list, stdlist, radio_list)\n\
+            root.title(stdpath)\n\
+        except:\n\
+            btn_list = dop.btn_lists(stdlist)\n\
+            radio_list = dop.radio_lists(stdlist)\n\
+            stdpath = dop.concat(dop.got_back(stdpath))\n\
+            stdlist = dop.sortdir(listdir(path=stdpath))\n\
+            print(stdpath)\n\
+            destr(btn_list, stdlist, radio_list)\n\
+def radio_click{1}(event):\n\
+    print({1})\n\
+{0} = Button(right, text=stdlist[i], bg='old lace')\n\
+{0}.bind('<Button-1>', left_click{1})\n\
+{0}.place(x=10, y=rast)\n\
+var{1} = BooleanVar()\n\
+var{1}.set(0)\n\
+{2} = Checkbutton(right, variable=var{1}, onvalue=1, offvalue=0,bg='lemon chiffon', command=radio_click{1})\n\
+{2}.bind('<Button-1>', radio_click{1})\n\
+{2}.place(x=10, y=rast)\n\
+"
+
 def sortdir(arg):
     x1, x2 = [i for i in arg if not "." in i], [i for i in arg if "." in i]
     return x1 + x2
@@ -10,7 +45,7 @@ def mycopy(arg):
 def concat(arg):
     return arg + "\\"
 
-def got_back(arg, x):
+def got_back(arg, x="\\"):
     g = str_to_list(arg)[::-1]
     c = "".join(take_while(x, g[1:])[::-1])
     return c
@@ -24,3 +59,17 @@ def take_while(f, arg):
     for i in range(len(arg)):
         if "".join(x[i:i+1]) == f:
             return x[i+1:]
+
+def btn_lists(stdlist):
+    btn_list = []
+    for i in range(len(stdlist)):
+        name_btn = "btn" + str(i)
+        btn_list.append(name_btn)
+    return btn_list
+
+def radio_lists(stdlist):
+    btn_list = []
+    for i in range(len(stdlist)):
+        name_btn = "radio" + str(i)
+        btn_list.append(name_btn)
+    return btn_list
--- fm.py
+++ fm.py
@@ -13,24 +13,6 @@ import dop # Мой дополнительный модуль
 
 stdpath = dop.concat(getenv("SystemDrive"))
 stdlist = dop.sortdir(listdir(path=stdpath))
-btn_list = []
-
-execute = "\n\
-global {0}\n\
-def left_click{1}(event):\n\
-    global stdpath, stdlist\n\
-    if '.' in stdlist[{1}]:\n\
-        pass\n\
-    else:\n\
-        stdpath = dop.concat(path.abspath(stdpath + stdlist[{1}]))\n\
-        print(stdpath)\n\
-        stdlist = dop.sortdir(listdir(path=stdpath))\n\
-        destr(btn_list, stdlist)\n\
-        root.title(stdpath)\n\
-{0} = Button(right, text=stdlist[i], bg='old lace')\n\
-{0}.bind('<Button-1>', left_click{1})\n\
-{0}.place(x=10, y=rast)\n\
-"
 
 def per(event):
     upd()
@@ -39,39 +21,46 @@ def vn(event):
     upd()
 
 def get_back():
-    global stdpath
+    global stdpath, stdlist
+    btn_list = dop.btn_lists(stdlist)
+    radio_list = dop.radio_lists(stdlist)
     stdpath = dop.concat(dop.got_back(stdpath, "\\"))
-    destr(stdlist, stdpath)
-    upd()
+    stdlist = dop.sortdir(listdir(path=stdpath))
+    destr(btn_list, stdlist, radio_list)
 
 def upd():
     global left, right, line
     
-    left.config(height=root.winfo_height()-80)
     right.config(height=root.winfo_height()-80)
 
     line.config(to=len(stdlist), length=root.winfo_height()-85)
-    line.place(x=root.winfo_width()-380)
+    line.place(x=root.winfo_width()-40)
 
     rast = 30 - int(line.get()) * 30
     for i in range(len(stdlist)):
         name_btn = "btn" + str(i)
+        name_radio = "radio" + str(i)
         exec(
-"{0}.place(x=10, y=rast)\n\
-".format(name_btn))
+"{0}.place(x=25, y=rast)\n\
+{1}.place(x=1, y=rast)\n\
+".format(name_btn, name_radio))
         rast += 30
 
-def destr(x, y):
+def destr(x, y, z):
     global rast
     rast = 5
     for i in x:
         exec(
 "{0}.destroy()\n\
 ".format(i))
+    for i in z:
+        exec(
+"{0}.destroy()\n\
+".format(i))
     for i in range(len(y)):
         name_btn = "btn" + str(i)
-        btn_list.append(name_btn)
-        exec(execute.format(name_btn, i))
+        name_radio = "radio" + str(i)
+        exec(dop.exec().format(name_btn, i, name_radio))
         rast += 30
         
 root = Tk()
@@ -79,11 +68,8 @@ root.geometry("1000x600")
 root.minsize(1000, 600)
 root.title(stdpath)
 
-left = Frame(root, width=333, height=560, bg="gray76")
-left.place(x=0, y=80)
-
 right = Frame(root, width=4000, height=560, bg="lemon chiffon")
-right.place(x=333, y=80)
+right.place(x=0, y=80)
 
 line = Scale(right, from_ = 1, to = len(stdlist), bg="lemon chiffon")
 line.place(x=1, y=0)
@@ -92,10 +78,12 @@ back = Button(root, text="<-", font="Arial 30", fg="ivory4", command=get_back)
 back.place(x=0, y=0)
 
 rast = 5
+btn_list = dop.btn_lists(stdlist)
+radio_list = dop.radio_lists(stdlist)
 for i in range(len(stdlist)):
     name_btn = "btn" + str(i)
-    btn_list.append(name_btn)
-    exec(execute.format(name_btn, i))
+    name_radio = "radio" + str(i)
+    exec(dop.exec().format(name_btn, i, name_radio))
     rast += 30
 
 line.bind("<B1-Motion>", vn)
