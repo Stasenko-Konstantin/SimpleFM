c0c49fdc3a79fde258ffd3819cf124285db1c96e
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2021-02-20 12:43:20 +0500

    a?

--- fm.py
+++ fm.py
@@ -1,150 +1,224 @@
-from tkinter import *
-from tkinter.filedialog import *
-import os
+# -*- coding: utf-8 -*-
+#! Главный модуль
 
-sc = 0
-data = []
-labels = {}
+'''
+Автор: Стасенко Константин
+О программе: простой файловый менеджер который необходимо было сделать по учебному
+заданию
+'''
+
+from tkinter import *
+import os
+from shutil import copytree, copyfile
+import dop # Мой дополнительный модуль
+
+class Exec:
+
+    @staticmethod
+    def copy(n):
+        global copyname
+        copyname = os.path.abspath(stdpath + stdlist[n])
+
+    @staticmethod
+    def renok(n):
+        renamename = os.path.abspath(stdpath + stdlist[n])
+        try:
+            error.config(text=" ")
+            os.rename(renamename, stdpath+ren.get())
+            upd2()
+        except:
+            error.config(text="Неверное имя")
+        ren.destroy()
+
+    @staticmethod        
+    def rename(n):
+        global ren
+        ren = Entry(root)
+        ren.place(y=20, x=400)
+        root.bind('<Return>', lambda event: Exec.renok(n))
+
+    @staticmethod        
+    def delete(n):
+        if os.path.isfile(os.path.abspath(stdpath + stdlist[n])):
+            os.remove(os.path.abspath(stdpath + stdlist[n]))
+        else:
+            os.rmdir(os.path.abspath(stdpath + stdlist[n]))
+        upd2()
+
+    @staticmethod
+    def popup(event, menu):
+        menu.post(event.x_root, event.y_root)
+
+    @staticmethod
+    def left_click(n):
+        global stdpath, stdlist
+        if os.path.isfile(os.path.abspath(stdpath + stdlist[n])):
+            os.startfile(os.path.abspath(stdpath + stdlist[n]))
+        else:
+            try:
+                stdpath = dop.concat(os.path.abspath(stdpath + stdlist[n]))
+                btn_list = dop.btn_lists(stdlist)
+                stdlist = dop.sortdir(os.listdir(path=stdpath))
+                destr(btn_list, stdlist)
+                root.title(stdpath)
+            except:
+                btn_list = dop.btn_lists(stdlist)
+                stdpath = dop.concat(dop.got_back(stdpath, "\\"))
+                stdlist = dop.sortdir(os.listdir(path=stdpath))
+                destr(btn_list, stdlist)
+
+    @staticmethod
+    def exec(name_btn, i, rast):
+        global btn
+        btns[name_btn] = Button(right, text=stdlist[i], bg='old lace')
+        btns[name_btn].place(x=10, y=rast)
+        menu = Menu(tearoff=0)
+        menu.add_command(label='Копировать', command= lambda: Exec.copy(i))
+        menu.add_command(label='Переименовать', command= lambda: Exec.rename(i))
+        menu.add_command(label='Удалить', command= lambda: Exec.delete(i))
+        btns[name_btn].bind('<Double-Button-1>', lambda event: Exec.left_click(i))
+        btns[name_btn].bind('<Button-3>', lambda event: Exec.popup(event, menu))
+
+stdpath = dop.concat(os.getenv("SystemDrive"))
+stdlist = dop.sortdir(os.listdir(path=stdpath))
+btns = {}
 ch = 0
 
-class Customer:
-    def __init__(self, name):
-        self.name = name
-        self.products = {}
-    
-    def addProduct(self, product, price):
-        self.products[product] = price
-    
-    def getProducts(self):
-        return self.products
-    
-    def getProduct(self, product):
-        return self.products[product]
+def popup(event):
+    menu.post(event.x_root, event.y_root)
 
-#Всплывающие окна при нажатии кнопки "Помощь"
-def Help():
-    root1 = Tk()
-    root1.title('Помощь')
-    root1.geometry('420x100')
-    lbl = Label(root1, text="Для работы с программой\n\
-вам просто нужно ввести отпускную цену\n\
-для каждого вида обуви. Программа, на основе\n\
-введеных данных, сама посчитает доход и выведет диаграмму.", font="Arial 10")
-    lbl.place(x=10, y=10)
-    root1.mainloop()
+def paste():
+    if os.path.isfile(copyname):
+        two = (dop.list_to_str(dop.take_while2("\\", copyname[::-1])[::-1]))
+        print(stdpath[:-1])
+        print(two)
+        copyfile(copyname, stdpath[:-1]+two)
+    else:
+        two = (dop.list_to_str(dop.take_while2("\\", copyname[::-1])[::-1]))
+        print(stdpath[:-1])
+        print(two)
+        copytree(copyname, stdpath[:-1]+two)
+    upd2()
 
+def createfile():
+    global cen
+    cen = Entry(root)
+    cen.place(y=20, x=400)
+    root.bind('<Return>', createfile2)
 
-#Всплывающие окна при нажатии кнопки "О Программе"
-def About():
-    root2 = Tk()
-    root2.title('О Программе')
-    root2.geometry('270x100')
-    lbl = Label(root2, text="Программа: Project v1.0\n\
-Разработана ТАТК ГА филиал МГТУ ГА\n\
-курсантом 331 группы Мешалкиным В.О", font="Arial 10")
-    lbl.place(x=10, y=10)
-    root2.mainloop()
-
-def create(i, rast):
-    pass
-
-def click(event, i, rast):
-    pass
-
-#Таблица
-def fill_table():
-    pass
-
-def oppen():
-    global data
-    '''path = askopenfilename()
+def createfile2(event):
     try:
-        with open(path, 'r', encoding='utf-8') as f:
-            f = f.read()
+        error.config(text=" ")
+        f = open(str(stdpath+cen.get()), "w")
+        f.write(" ")
+        f.close()
+        upd2()
     except:
-        oppen()
-        return'''
-    #Покупатель Товар    Кол-ва
-    #ФИО        Название Тут ясно
-    f = '{"Чувак1": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак2": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак3": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак4": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак5": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак6": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак7": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак8": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак9": [{"панталоны": 500}, {"картина": 12}]}\n\
-{"Чувак10": [{"панталоны": 500}, {"картина": 12}]}'
-    temp = f.split('\n')
-    for i in range(len(temp)):
-        name_border = temp[i][1:].index(':')
-        products = temp[i][temp[i].index('[')+1:temp[i].index(']')].split(', ')
-        customer = Customer(temp[i][2:name_border])
-        data.append(customer)
-        for j in range(len(products)):
-            prod_border = products[j][2:].index('"')+2
-            prod = products[j][2:prod_border]
-            price_border = products[j].index('}')
-            price = products[j][prod_border+2:price_border]
-            data[i].addProduct(prod, int(price))
-    fill_table()
-    
-    
+        error.config(text='Неверное имя')
+    cen.destroy()
 
-def save():
-    pass
+def createdir():
+    global cen
+    cen = Entry(root)
+    cen.place(y=20, x=400)
+    root.bind('<Return>', createdir2)
+
+def createdir2(event):
+    try:
+        error.config(text=" ")
+        os.makedirs(stdpath+cen.get())
+        upd2()
+    except:
+        error.config(text='Неверное имя')
+    cen.destroy()
+        
+def per(event):
+    global ch
+    if event.delta < 0:
+        ch = 1
+    elif event.delta > 0:
+        ch = -1
+    upd()
+
+def get_back():
+    global stdpath, stdlist
+    btn_list = dop.btn_lists(stdlist)
+    stdpath = dop.concat(dop.got_back(stdpath, "\\"))
+    stdlist = dop.sortdir(os.listdir(path=stdpath))
+    destr(btn_list, stdlist)
+
+def upd2():
+    global stdpath, stdlist
+    btn_list = dop.btn_lists(stdlist)
+    stdpath = dop.concat(stdpath)
+    stdlist = dop.sortdir(os.listdir(path=stdpath))
+    destr(btn_list, stdlist)
+    upd()
 
 def upd():
-    global right, line, prast, ch, panel
+    global left, right, line, rast, ch, panel
     
-    panel.config(width=root.winfo_width(), height=root.winfo_height())
-    right.config(width=root.winfo_width()-80)
+    panel.config(width=root.winfo_width(), height=root.winfo_height()-80)
+    right.config(width=root.winfo_width())
 
-    line.config(to=len(data), length=root.winfo_height())
+    line.config(to=len(stdlist), length=root.winfo_height()-85)
     line.place(x=root.winfo_width()-40)
 
     ref = line.get()
     line.set(ref + ch)
     ch = 0
     ref = line.get()
-    prast = 30 - ref * 30
+    rast = 30 - ref * 30
     
-    right.place(x=10, y=prast)
+    right.place(x=10, y=rast)
+
+def destr(x, y):
+    global rast, btns
+    rast = 5
+    for i in x:
+        btns[i].destroy()
+    for i in range(len(y)):
+        name_btn = "btn" + str(i)
+        Exec.exec(name_btn, i, rast)
+        rast += 30
 
 root = Tk()
-root.minsize(450, 450)
-root.resizable(False, False)
-root.config(bg="lemon chiffon")
-root.title("Program")
-mainmenu = Menu(root)
-root.config(menu=mainmenu)
+root.geometry("1000x600")
+root.minsize(1000, 600)
+root.title(stdpath)
 
-panel = Frame(root, width=400, height=600, bg="green")
-panel.place(x=0, y=0)
+error = Label(root, text=" ")
+error.place(y=20, x=450)
 
-right = Frame(panel, width=400, height=9999999, bg="lemon chiffon")
-right.place(x=25, y=0)
+panel = Frame(root, width=1000, height=600, bg="lemon chiffon")
+panel.place(x=0, y=80)
 
-oppen()
-line = Scale(panel, from_ = 1, to = len(data), bg="lemon chiffon")
+right = Frame(panel, width=4000, height=9999999, bg="lemon chiffon")
+right.place(x=0, y=80)
+
+line = Scale(panel, from_ = 1, to = len(stdlist), bg="lemon chiffon")
 line.place(x=1, y=0)
-upd()
 
-filemenu = Menu(mainmenu, tearoff=0)
-filemenu.add_command(label="Открыть", command=oppen)
-filemenu.add_command(label="Сохранить", command=save)
-filemenu.add_command(label="Выход", command=lambda: exit(1))
+back = Button(root, text="<-", font="Arial 30", fg="ivory4", command=get_back)
+back.place(x=0, y=0)
 
-helpmenu = Menu(mainmenu, tearoff=0)
-helpmenu.add_command(label="Помощь", command=Help)
-helpmenu.add_command(label="О Программе", command=About)
+menu = Menu(tearoff=0)
+menu.add_command(label="Вставить", command=paste)
+menu.add_command(label="Создать файл", command=createfile)
+menu.add_command(label="Создать папку", command=createdir)
 
-mainmenu.add_cascade(label="Файл", menu=filemenu)
-mainmenu.add_cascade(label="Помощь", menu=helpmenu)
+rast = 5
+btn_list = dop.btn_lists(stdlist)
+for i in range(len(stdlist)):
+    name_btn = "btn" + str(i)
+    Exec.exec(name_btn, i, rast)
+    rast += 30
 
+right.bind("<Button-3>", popup)
 line.bind("<B1-Motion>", lambda event: upd())
 line.bind("<Button-1>", lambda event: upd())
 line.bind("<ButtonRelease>", lambda event: upd())
+root.bind("<Configure>", lambda event: upd())
+#root.bind("<MouseWheel>", per)
 
 root.mainloop()
